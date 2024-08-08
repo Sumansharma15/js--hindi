72e5e378d6e88adae798ea7e3c19ccb35adb92a2
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-08-08 14:29:08 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+let score = "33" //quotes k andr string bnn jata hai otherwise no.
+
+//console.log(typeof score);
+
+ 
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//"33" => 33 ie string easily converted into number
+//"33nabc" => Nan
+//true = 1; false = 0;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0=> false
+//  "" =>  false
+// "suman" => true
+
+let somenumber = 15
+let stringNumber = String(somenumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
