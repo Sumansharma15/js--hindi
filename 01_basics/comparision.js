1603baeec4183d322d0d2cf6f4a1c64c19bd2d41
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-08-08 10:10:21 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,15 @@
+console.log("2">1); //here it is true bcz javascript automatically treat this string as number.
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null >= 0);// true bcz >= ie comparision treat null as zero thats is it is true.
+console.log(null == 0);
+
+
+//===  it is called strict check
+
+console.log("2"===1);
+
+
+
+
