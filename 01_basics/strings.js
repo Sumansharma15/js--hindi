078a699a46b0f0b32154409a740d2bcc9333e4a3
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-09-04 18:03:25 +0000

    done

---
+++ 01_basics/strings.js
@@ -0,0 +1,13 @@
+const name = "suman"
+const Value = 50
+//console.log(name + Value + "done");
+
+const gameName = new String('sumansh')
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+
