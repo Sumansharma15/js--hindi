d620f6fb3d802c6569ea4a975925a2092b053777
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-09-05 17:24:34 +0000

    math and num

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 300
+//console.log(score);
+const balance = new Number(200)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+const other = 124.55
+//console.log(other.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++maths*******
+
+console.log(Math);
+console.log(Math.abs(-5)); // minus wali plus
+console.log(Math.round(5.7))
+
+console.log(Math.random()); //.random se 0 aur 1 k beech values milti hain
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+
+
+
+
+
