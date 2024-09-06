fb3e136b93c86d781cb6bd9a01907e340be1ea48
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-09-06 17:33:22 +0000

    Dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,15 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024,9,6)
+console.log(myCreatedDate.toString());
+
+
+
+
+
