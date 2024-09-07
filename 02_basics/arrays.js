7e1b6b548733dfd18f84233bb352334e98fdca36
Sumansharma15 <165474615+Sumansharma15@users.noreply.github.com>
2024-09-07 16:17:51 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,22 @@
+// arrays
+
+const myArr = [1, 2, 3, 4, 5]
+const Heroes = ["shakti", "raj"]
+
+const myArr2 = new Array(1, 2, 23, 44)
+console.log(myArr[2]);
+
+//arrays methods//
+
+//myArr.push(7)
+//myArr.push(8)
+//myArr.pop()
+
+//myArr.unshift(3)
+myArr.shift()
+console.log(myArr.includes(9));
+
+console.log(myArr);
+// slice and splice in arrays
+
+
